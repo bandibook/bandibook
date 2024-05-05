7bb010047a68663a609271f7a873665942623eb3
Doeunnkimm <doeunnkimm@gmail.com>
2024-05-06 02:02:20 +0900

    chore: init

---
+++ theme.config.jsx
@@ -0,0 +1,13 @@
+export default {
+  logo: <span>반디북</span>,
+  project: {
+    link: 'https://github.com/bandibook/bandibook',
+  },
+  head: (
+    <>
+      <link rel="icon" href="/favicon.ico" />
+      <title>반디북 - 반디부디의 책지도</title>
+      <meta property="og:title" content="반디북 - 반디부디의 책지도" />
+    </>
+  ),
+};

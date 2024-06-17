c8c7ea481e2a3c8086fcb2e06bccd5cafe8a51f4
wkwon <rnjsdntjr26@daum.net>
2024-06-17 21:21:05 +0900

    feat(#20): 개인 공부 자동 병합 job과 workflow 작업

---
+++ combiner/cache/local_cache.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+
+class LocalCache:
+    def __init__(self, cache_file):
+        self.cache_file = cache_file
+        self.cache_data = self.__load_cache()
+
+    def __save_cache(self):
+        with open(self.cache_file, 'w', encoding='utf-8') as f:
+            json.dump(self.cache_data, f)
+
+    def update_cache(self, book_name, file_name, mod_time, reviewer_count):
+        if book_name not in self.cache_data:
+            self.cache_data[book_name] = {}
+
+        if file_name not in self.cache_data[book_name]:
+            self.cache_data[book_name][file_name] = {}
+
+        self.cache_data[book_name][file_name]["mod_time"] = mod_time
+        self.cache_data[book_name][file_name]["reviewer_count"] = reviewer_count
+        self.__save_cache()
+
+    def need_update(self, book_name, file_name, mod_time, reviewer_count):
+        if self.get_study_reviewer_count(book_name, file_name) != reviewer_count or self.get_file_mod_time(book_name, file_name) != mod_time:
+                return True
+
+        return False
+
+    def get_file_mod_time(self, book_name, file_name):
+        return self.cache_data.get(book_name, {}).get(file_name, {}).get("mod_time")
+
+    def get_study_reviewer_count(self, book_name, file_name):
+        return self.cache_data.get(book_name, {}).get(file_name, {}).get("reviewer_count")
+
+    def __load_cache(self):
+        if os.path.exists(self.cache_file):
+            with open(self.cache_file, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        return {}

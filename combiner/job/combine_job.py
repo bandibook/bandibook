b7212f407fb4de802b55433444166029e2f819d0
binary-ho <dfghcvb11@naver.com>
2024-11-24 03:00:16 +0900

    계층 정정

--- combiner/job/combine_job.py
+++ combiner/job/combine_job.py
@@ -5,22 +5,28 @@ from combiner.service.copier import FileCopier
 from combiner.utils.config_util import ConfigUtil
 
 class CombineJob:
-    def __init__(self, individual_study_dir, combine_dir, cache_file):
-        self.individual_study_dir = individual_study_dir
+    def __init__(self, root_dir, combine_dir, cache_file):
+        self.root_dir = root_dir
         self.combine_dir = combine_dir
-        self.combiner = Combiner(individual_study_dir, combine_dir, cache_file)
-        self.file_copier = FileCopier(individual_study_dir, combine_dir, cache_file)
+        self.combiner = Combiner(root_dir, combine_dir, cache_file)
+        self.file_copier = FileCopier(root_dir, combine_dir, cache_file)
 
     def run(self):
-        book_names = Parser.find_all_directories(self.individual_study_dir)
+        study_names = Parser.find_all_directories(self.root_dir)
+
+        for study_name in study_names:
+           self.combine_study(study_name)
+
+    def combine_study(self, study_name):
+        book_names = Parser.find_all_directories(self.root_dir, study_name)
 
         for book_name in book_names:
-            book_reviews_path = Parser.join_paths(self.individual_study_dir, book_name)
+            study_book_name = Parser.join_paths(study_name, book_name)
+            book_path = Parser.join_paths(self.root_dir, study_book_name)
             
-            if ConfigUtil.requires_review(book_reviews_path):
-                reviewer_count = len(Parser.find_all_directories(book_reviews_path))
-                all_finished_files = Parser.find_common_files(book_reviews_path)
-                self.combiner.combine(all_finished_files, book_reviews_path, book_name, reviewer_count)
+            if ConfigUtil.requires_review(book_path):
+                reviewer_count = len(Parser.find_all_directories(book_path))
+                all_finished_files = Parser.find_common_files(book_path)
+                self.combiner.combine(all_finished_files, book_path, study_book_name, reviewer_count)
             else:
-                self.file_copier.copy_files(book_name)
-                
+                self.file_copier.copy_files(study_book_name)
--- combiner/service/copier.py
+++ combiner/service/copier.py
@@ -7,20 +7,13 @@ from combiner.utils.parser import Parser
 class FileCopier:
     NO_REVIEWER = 0
 
-    def __init__(self, individual_study_dir, combine_dir, cache_file):
-        self.individual_study_dir = individual_study_dir
+    def __init__(self, root_dir, combine_dir, cache_file):
+        self.root_dir = root_dir
         self.combine_dir = combine_dir
         self.cache = LocalCache(cache_file)
 
-    def copy_files(self, book_name):
-        directories = Parser.find_all_directories(self.individual_study_dir, book_name)
-
-        for directory in directories:
-            source_path = os.path.join(book_name, directory)
-            self.__copy_files(source_path)    
-
-    def __copy_files(self, source_path):
-        files = Parser.find_all_files(self.individual_study_dir, source_path)
+    def copy_files(self, source_path):
+        files = Parser.find_all_files(self.root_dir, source_path)
 
         for file_name in files:
             file_mod_time = self.__get_file_mod_time(source_path, file_name)
@@ -33,9 +26,9 @@ class FileCopier:
         destination_dir = os.path.join(self.combine_dir, source_path)
         os.makedirs(destination_dir, exist_ok=True)
 
-        source_file = os.path.join(self.individual_study_dir, source_path, file_name)
+        source_file = os.path.join(self.root_dir, source_path, file_name)
         shutil.copy2(source_file, destination_dir)
 
     def __get_file_mod_time(self, source_path, file_name):
-        file_path = os.path.join(self.individual_study_dir, source_path, file_name)
+        file_path = os.path.join(self.root_dir, source_path, file_name)
         return os.path.getmtime(file_path)

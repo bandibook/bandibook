8d34194b54b5bb2ec71d3294431bdb34a9dd65ee
Doeun Kim <doeunnkimm@gmail.com>
2024-11-24 01:38:59 +0900

    chore: 반디북과 만베거를 분리하여 관리할 수 있도록 확장 (#99)
    * 10000-bagger-init
    
    * chore: update CODEOWNERS

--- constants/members.ts
+++ constants/members.ts
@@ -4,25 +4,34 @@ export interface MembersInfo {
   github: string;
 }
 
-export const membersInfo: Array<MembersInfo> = [
-  {
-    username: 'Doeunnkimm',
-    profileUrl: 'https://avatars.githubusercontent.com/u/112946860?v=4',
-    github: 'https://github.com/Doeunnkimm',
-  },
-  {
-    username: 'Heojoooon',
-    profileUrl: 'https://avatars.githubusercontent.com/u/45158550?v=4',
-    github: 'https://github.com/hjy0951',
-  },
-  {
-    username: 'ManHyuk',
-    profileUrl: 'https://avatars.githubusercontent.com/u/11765448?v=4',
-    github: 'https://github.com/ManHyuk',
-  },
-  {
-    username: '권우석',
-    profileUrl: 'https://avatars.githubusercontent.com/u/62459196?v=4',
-    github: 'https://github.com/egg528',
-  },
-];
+export const membersInfo: Record<string, Array<MembersInfo>> = {
+  bandibook: [
+    {
+      username: 'Doeunnkimm',
+      profileUrl: 'https://avatars.githubusercontent.com/u/112946860?v=4',
+      github: 'https://github.com/Doeunnkimm',
+    },
+    {
+      username: 'Heojoooon',
+      profileUrl: 'https://avatars.githubusercontent.com/u/45158550?v=4',
+      github: 'https://github.com/hjy0951',
+    },
+    {
+      username: 'ManHyuk',
+      profileUrl: 'https://avatars.githubusercontent.com/u/11765448?v=4',
+      github: 'https://github.com/ManHyuk',
+    },
+    {
+      username: '권우석',
+      profileUrl: 'https://avatars.githubusercontent.com/u/62459196?v=4',
+      github: 'https://github.com/egg528',
+    },
+  ],
+  '10000-bagger': [
+    {
+      username: 'binary_ho',
+      profileUrl: 'https://avatars.githubusercontent.com/u/71186266?v=4',
+      github: 'https://github.com/binary-ho',
+    },
+  ],
+};
